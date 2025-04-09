2a511877b3e0de4f73458672f399aca373010c3a
Caleb <m253300@usna.edu>
2025-04-09 19:58:46 -0400

    i converted it to a frustrum

--- lab11/part3.py
+++ lab11/part3.py
@@ -19,19 +19,27 @@ class Scene:
         # Define how we should draw whats inside the window
         @self.window.event
         def on_draw():
+            # glMatrixMode(gl.GL_PROJECTION)
+            # glLoadIdentity()
+            # glOrtho(-cols/2, cols/2, -rows/2, rows/2, -20.0, 0)
+            # glMatrixMode(gl.GL_MODELVIEW)
+            # glLoadIdentity()
+
+            # glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+            # glLoadIdentity()
+
+            self.window.clear()
+
+            glViewport(0, 0, width, height)
             glMatrixMode(gl.GL_PROJECTION)
             glLoadIdentity()
-            glOrtho(-cols/2, cols/2, -rows/2, rows/2, -20.0, 0)
+            glFrustum(-cols/4, cols/4, -rows/4, rows/4, 0.5, 20)
             glMatrixMode(gl.GL_MODELVIEW)
-            glLoadIdentity()
 
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
             glLoadIdentity()
 
-            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-            glLoadIdentity()
-
-            gluLookAt(0, 0.0, 40, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+            #gluLookAt(0, 0.0, 40, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
             # draw the contour for each threshold 0.5-19.5 incrementing by 0.5
             for i in np.arange(0.5, 20.0, 1):
@@ -79,7 +87,7 @@ class Scene:
                         interp = interpolate(tl, tr, t)
                         x = tlc[0] + (j+interp)
                         y = tlc[1] - (i)
-                        glVertex3f(x, y, t)
+                        glVertex3f(x, y, -1)
 
                     # (i, j+1) - (i+1, j+1)
                     # tr - br
@@ -87,7 +95,7 @@ class Scene:
                         interp = interpolate(tr, br, t)
                         x = tlc[0] + (j+1)
                         y = tlc[1] - (i+interp)
-                        glVertex3f(x, y, t)
+                        glVertex3f(x, y, -1)
 
                     # (i+1, j+1) - (i+1, j)
                     # br - bl
@@ -95,7 +103,7 @@ class Scene:
                         interp = interpolate(bl, br, t)
                         x = tlc[0] + (j+interp)
                         y = tlc[1] - (i+1)
-                        glVertex3f(x, y, t)
+                        glVertex3f(x, y, -1)
 
                     # (i+1, j) - (i, j)
                     # bl - tl
@@ -103,11 +111,11 @@ class Scene:
                         interp = interpolate(tl, bl, t)
                         x = tlc[0] + (j)
                         y = tlc[1] - (i+interp)
-                        glVertex3f(x, y, t)
+                        glVertex3f(x, y, -1)
             glEnd()
 
 # Begin the main program loop
-map = map.get_matrix(seed = 3, rows = 10, cols = 10)
+map = map.get_matrix(seed = 3, rows = 100, cols = 60)
 rows = len(map)
 cols = len(map[0])
 myScene = Scene(600, 600)
