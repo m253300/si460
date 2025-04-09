436322fd8a9081bd50ecd17d6c4619a82e9cfe38
Caleb <m253300@usna.edu>
2025-04-09 19:45:31 -0400

    i think i need to replace the glOrtho with a equivalent glFrustrum

--- lab11/part3.py
+++ lab11/part3.py
@@ -6,84 +6,109 @@ from pyglet.gl import *
 import makeTopoMap as map
 import numpy as np
 
-# Define the window
-window = pyglet.window.Window(600, 600, resizable=False, caption='Koutrakos - Lab 11 - Part 3')
+class Scene:
+    def __init__(self, width=600, height=600, caption='Koutrakos - Lab 11 - Part 3', resizable=False):
 
-# Define how we should draw whats inside the window
-@window.event
-def on_draw():
-    glMatrixMode(gl.GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(-(rows-1)/2 - 1, (rows-1)/2 + 1, -(cols-1)/2 - 1, (cols-1)/2 + 1, -2.0, 1.0)
-    glMatrixMode(gl.GL_MODELVIEW)
-    glLoadIdentity()
+        self.cameraX = 0.0
+        self.cameraZ = -60.0
+        self.angle = 0.0
 
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
+        # Define the window
+        self.window = pyglet.window.Window(width=width, height=height, resizable=resizable, caption=caption)
 
-    # draw the contour for each threshold 0.5-19.5 incrementing by 0.5
-    for i in np.arange(0.5, 20.0, 1):
-        drawContour(i)
+        # Define how we should draw whats inside the window
+        @self.window.event
+        def on_draw():
+            glMatrixMode(gl.GL_PROJECTION)
+            glLoadIdentity()
+            glOrtho(-cols/2, cols/2, -rows/2, rows/2, -20.0, 0)
+            glMatrixMode(gl.GL_MODELVIEW)
+            glLoadIdentity()
 
-def interpolate(a, b, t):
-    return (t-a)/(b-a)
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+            glLoadIdentity()
 
-def drawContour(t):
-    glBegin(GL_LINES)
-    glColor3f(1.0, 1.0, 1.0)   # White
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+            glLoadIdentity()
 
-    tlc = (-(rows-1)/2, (cols-1)/2)
+            gluLookAt(0, 0.0, 40, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
-    for i in range(rows-1):
-        for j in range(cols-1):
-            # top-left (tl) - (i, j)
-            tl = map[i][j]
+            # draw the contour for each threshold 0.5-19.5 incrementing by 0.5
+            for i in np.arange(0.5, 20.0, 1):
+                drawContour(i, 0.3+0.05*(i-0.5))
 
-            # top-right (tr) - (i, j+1)
-            tr = map[i][j+1]
+        @self.window.event
+        def on_text_motion(motion):
+            #print(str(['key held down', "motion = ", motion]))
+            
+            if motion == 65363: # right arrow
+                self.angle += 1
+            elif motion == 65361: # left arrow
+                self.angle -= 1
+            
+            self.cameraX = 0.0 + 60.0*np.cos(self.angle*(np.pi/180))
+            self.cameraZ = 0.0 + 60.0*np.sin(self.angle*(np.pi/180))
 
-            # bottom-right (br) - (i+1, j+1)
-            br = map[i+1][j+1]
+        def interpolate(a, b, t):
+            return (t-a)/(b-a)
 
-            # bottom-left (bl) - (i+1, j)
-            bl = map[i+1][j]
+        def drawContour(t, color):
+            glBegin(GL_LINES)
+            glColor3f(color, color, color)   # Shades of gray
 
-            # check:
-            # (i, j) - (i, j+1)
-            # tl - tr
-            if tl < t < tr or tl > t > tr:
-                interp = interpolate(tl, tr, t)
-                x = tlc[0] + (j+interp)
-                y = tlc[1] - (i)
-                glVertex3f(x, y, 0.0)
+            tlc = (-(cols-1)/2, (rows-1)/2)
 
-            # (i, j+1) - (i+1, j+1)
-            # tr - br
-            if tr < t < br or tr > t > br:
-                interp = interpolate(tr, br, t)
-                x = tlc[0] + (j+1)
-                y = tlc[1] - (i+interp)
-                glVertex3f(x, y, 0.0)
+            for i in range(rows-1):
+                for j in range(cols-1):
+                    # top-left (tl) - (i, j)
+                    tl = map[i][j]
 
-            # (i+1, j+1) - (i+1, j)
-            # br - bl
-            if br < t < bl or br > t > bl:
-                interp = interpolate(bl, br, t)
-                x = tlc[0] + (j+interp)
-                y = tlc[1] - (i+1)
-                glVertex3f(x, y, 0.0)
+                    # top-right (tr) - (i, j+1)
+                    tr = map[i][j+1]
 
-            # (i+1, j) - (i, j)
-            # bl - tl
-            if bl < t < tl or bl > t > tl:
-                interp = interpolate(tl, bl, t)
-                x = tlc[0] + (j)
-                y = tlc[1] - (i+interp)
-                glVertex3f(x, y, 0.0)
-    glEnd()
+                    # bottom-right (br) - (i+1, j+1)
+                    br = map[i+1][j+1]
+
+                    # bottom-left (bl) - (i+1, j)
+                    bl = map[i+1][j]
+
+                    # check:
+                    # (i, j) - (i, j+1)
+                    # tl - tr
+                    if tl < t < tr or tl > t > tr:
+                        interp = interpolate(tl, tr, t)
+                        x = tlc[0] + (j+interp)
+                        y = tlc[1] - (i)
+                        glVertex3f(x, y, t)
+
+                    # (i, j+1) - (i+1, j+1)
+                    # tr - br
+                    if tr < t < br or tr > t > br:
+                        interp = interpolate(tr, br, t)
+                        x = tlc[0] + (j+1)
+                        y = tlc[1] - (i+interp)
+                        glVertex3f(x, y, t)
+
+                    # (i+1, j+1) - (i+1, j)
+                    # br - bl
+                    if br < t < bl or br > t > bl:
+                        interp = interpolate(bl, br, t)
+                        x = tlc[0] + (j+interp)
+                        y = tlc[1] - (i+1)
+                        glVertex3f(x, y, t)
+
+                    # (i+1, j) - (i, j)
+                    # bl - tl
+                    if bl < t < tl or bl > t > tl:
+                        interp = interpolate(tl, bl, t)
+                        x = tlc[0] + (j)
+                        y = tlc[1] - (i+interp)
+                        glVertex3f(x, y, t)
+            glEnd()
 
 # Begin the main program loop
-map = map.get_matrix(rows = 10, cols = 10, seed = 3, delta = 3, maxval = 20)
+map = map.get_matrix(seed = 3, rows = 10, cols = 10)
 rows = len(map)
 cols = len(map[0])
+myScene = Scene(600, 600)
 pyglet.app.run()
