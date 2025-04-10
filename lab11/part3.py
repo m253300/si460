905bd45cece7db2c8d2cd1288e304d1eea0cd532
Caleb <m253300@usna.edu>
2025-04-09 20:09:28 -0400

    reverted back to ortho bc f frustum

--- lab11/part3.py
+++ lab11/part3.py
@@ -9,53 +9,37 @@ import numpy as np
 class Scene:
     def __init__(self, width=600, height=600, caption='Koutrakos - Lab 11 - Part 3', resizable=False):
 
-        self.cameraX = 0.0
-        self.cameraZ = -60.0
-        self.angle = 0.0
-
         # Define the window
         self.window = pyglet.window.Window(width=width, height=height, resizable=resizable, caption=caption)
 
         # Define how we should draw whats inside the window
         @self.window.event
         def on_draw():
+            # self.window.clear()
+
+            # glViewport(0, 0, width, height)
             # glMatrixMode(gl.GL_PROJECTION)
             # glLoadIdentity()
-            # glOrtho(-cols/2, cols/2, -rows/2, rows/2, -20.0, 0)
+            # glFrustum(-cols/4, cols/4, -rows/4, rows/4, 0.5, 20)
             # glMatrixMode(gl.GL_MODELVIEW)
-            # glLoadIdentity()
 
             # glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
             # glLoadIdentity()
 
-            self.window.clear()
-
-            glViewport(0, 0, width, height)
             glMatrixMode(gl.GL_PROJECTION)
             glLoadIdentity()
-            glFrustum(-cols/4, cols/4, -rows/4, rows/4, 0.5, 20)
+            glOrtho(-cols/2, cols/2, -rows/2, rows/2, -20.0, 0)
             glMatrixMode(gl.GL_MODELVIEW)
+            glLoadIdentity()
 
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
             glLoadIdentity()
 
-            #gluLookAt(0, 0.0, 40, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+            gluLookAt(0.0, 0.0, 2, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
             # draw the contour for each threshold 0.5-19.5 incrementing by 0.5
             for i in np.arange(0.5, 20.0, 1):
                 drawContour(i, 0.3+0.05*(i-0.5))
-
-        @self.window.event
-        def on_text_motion(motion):
-            #print(str(['key held down', "motion = ", motion]))
-            
-            if motion == 65363: # right arrow
-                self.angle += 1
-            elif motion == 65361: # left arrow
-                self.angle -= 1
-            
-            self.cameraX = 0.0 + 60.0*np.cos(self.angle*(np.pi/180))
-            self.cameraZ = 0.0 + 60.0*np.sin(self.angle*(np.pi/180))
 
         def interpolate(a, b, t):
             return (t-a)/(b-a)
@@ -87,7 +71,7 @@ class Scene:
                         interp = interpolate(tl, tr, t)
                         x = tlc[0] + (j+interp)
                         y = tlc[1] - (i)
-                        glVertex3f(x, y, -1)
+                        glVertex3f(x, y, t)
 
                     # (i, j+1) - (i+1, j+1)
                     # tr - br
@@ -95,7 +79,7 @@ class Scene:
                         interp = interpolate(tr, br, t)
                         x = tlc[0] + (j+1)
                         y = tlc[1] - (i+interp)
-                        glVertex3f(x, y, -1)
+                        glVertex3f(x, y, t)
 
                     # (i+1, j+1) - (i+1, j)
                     # br - bl
@@ -103,7 +87,7 @@ class Scene:
                         interp = interpolate(bl, br, t)
                         x = tlc[0] + (j+interp)
                         y = tlc[1] - (i+1)
-                        glVertex3f(x, y, -1)
+                        glVertex3f(x, y, t)
 
                     # (i+1, j) - (i, j)
                     # bl - tl
@@ -111,11 +95,11 @@ class Scene:
                         interp = interpolate(tl, bl, t)
                         x = tlc[0] + (j)
                         y = tlc[1] - (i+interp)
-                        glVertex3f(x, y, -1)
+                        glVertex3f(x, y, t)
             glEnd()
 
 # Begin the main program loop
-map = map.get_matrix(seed = 3, rows = 100, cols = 60)
+map = map.get_matrix(seed = 3, rows = 10, cols = 10)
 rows = len(map)
 cols = len(map[0])
 myScene = Scene(600, 600)
